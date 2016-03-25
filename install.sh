c52aac03383cb8a86d9b166991871bdd276c5353
Kristan Webb <kristan@webbitpro.co.uk>
2016-03-25 14:37:30 +0000

    tidy up

--- install.sh
+++ install.sh
@@ -32,12 +32,34 @@ echo "Install some stuff"
 
 sudo dpkg-reconfigure ssh
 
-sudo apt-get install -y scrot lsb-core fbi uzbl matchbox-window-manager supervisor python-pip python-dev python-simplejson python-imaging uzbl sqlite3 omxplayer x11-xserver-utils libx11-dev watchdog chkconfig
+sudo apt-get install -y scrot lsb-core fbi uzbl matchbox-window-manager supervisor x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3 python-pip python-dev python-simplejson python-imaging uzbl sqlite3 omxplayer x11-xserver-utils libx11-dev watchdog chkconfig
 
+echo "Install Hamachi"
+wget https://secure.logmein.com/labs/logmein-hamachi_2.1.0.139-1_armhf.deb
+sudo dpkg -i logmein-hamachi_2.1.0.139-1_armhf.deb 
+sudo hamachi login
+sudo hamachi attach kristan@webbitpro.co.uk
+
+echo "Configure ntp"
+#http://raspberrypi.stackexchange.com/questions/266/how-can-i-keep-system-time
+#https://www.raspberrypi.org/forums/viewtopic.php?f=91&t=16058
+sudo echo "DAEMONS(!hwclock ntpd ntpdate)" >> /etc/rc.conf
+sudo ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+
+echo "Python stuff"
 
 sudo pip install Mako MarkupSafe PyHAML bottle bottle-haml hurry.filesize netifaces pytz requests sh==1.08 six uptime wsgiref werzeug
 
+
+echo "Install player"
 git clone https://github.com/broadcasttechie/theatresignage-player-pi.git "$HOME/ts"
+
+
+echo "Configure player starup"
+#http://blogs.wcode.org/2013/09/howto-boot-your-raspberry-pi-into-a-fullscreen-browser-kiosk/
+
+
+
 
 
 echo "Increasing swap space to 500MB..."
@@ -69,6 +91,13 @@ if grep -q framebuffer_ignore_alpha /boot/config.txt; then
 else
   echo 'framebuffer_ignore_alpha=1' | sudo tee -a /boot/config.txt > /dev/null
 fi
+
+echo "Install boot splash"
+#http://www.edv-huber.com/index.php/problemloesungen/15-custom-splash-screen-for-raspberry-pi-raspbian
+sudo cp $HOME/ts/asplashscreen /etc/init.d/asplashscreen
+sudo cp pi_loading.sh /etc/splash.png
+sudo chmod a+x /etc/init.d/asplashscreen
+sudo insserv /etc/init.d/asplashscreen
 
 
 echo "Quiet the boot process..."
